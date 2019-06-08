320954b0c7a1fbbd1f77a6c79d431d3a6d2fcb98
Acid <clockstopfountain@gmail.com>
2019-06-08 06:57:49 +0530

    Updates

---
+++ precache-manifest.41c425c8d8b48c3d30d6e076bc1c29ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "412bc265e209191fb54a9a89097b96f6",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "79af57b7dbbfbe0a1d2a",
+    "url": "/my-app/static/css/main.b08aafa6.chunk.css"
+  },
+  {
+    "revision": "17179861716fc1c640fc",
+    "url": "/my-app/static/js/2.aa95d8ed.chunk.js"
+  },
+  {
+    "revision": "79af57b7dbbfbe0a1d2a",
+    "url": "/my-app/static/js/main.ad7aa44e.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "e993b2e4e99aeb423272e86ad13b4e80",
+    "url": "/my-app/static/media/jordan-whitfield-107093-unsplash.e993b2e4.jpg"
+  }
+]);
